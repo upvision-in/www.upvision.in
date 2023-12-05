92bde6985994b15143ab0224c1b2b3a5ba730a36
sumeet-upvision <sumeet@upvision.in>
2023-12-05 20:36:22 +0530

    added alternate workday for 2023-12-07

--- assets/js/rota/alternateWorkdays.js
+++ assets/js/rota/alternateWorkdays.js
@@ -2,5 +2,6 @@ function getAlternateWorkdays() {
   return [
     { name: 'H Jayesh', workDate: '2023-12-01' },
     { name: 'H Jayesh', workDate: '2023-12-02' },
+    { name: 'Vedant', workDate: '2023-12-07', shift: '2-Afternoon' },
   ];
 }
