235e84901e0cf797804f7a7d891c5605afb07b69
sumeet-upvision <sumeet@upvision.in>
2024-05-10 21:35:57 +0530

    updated holidays and internal holidays

--- assets/js/rota/holidays.js
+++ assets/js/rota/holidays.js
@@ -1,15 +1,14 @@
 function getHolidays() {
   return [
-    { date: '2023-10-4', name: 'abc' },
-    { date: '2024-1-14', name: 'Uttarayan' },
-    { date: '2024-1-26', name: 'Republic Day' },
-    { date: '2024-3-25', name: 'Dhuleti' },
-    { date: '2024-8-15', name: 'Independence Day' },
-    { date: '2024-8-19', name: 'Rakshabandhan' },
-    { date: '2024-8-26', name: 'Janmashtami' },
+    { date: '2024-01-14', name: 'Uttarayan' },
+    { date: '2024-01-26', name: 'Republic Day' },
+    { date: '2024-03-25', name: 'Dhuleti' },
+    { date: '2024-08-15', name: 'Independence Day' },
+    { date: '2024-08-19', name: 'Rakshabandhan' },
+    { date: '2024-08-26', name: 'Janmashtami' },
     { date: '2024-10-12', name: 'Dusshera' },
     { date: '2024-10-31', name: 'Diwali' },
-    { date: '2024-11-2', name: 'New Year (India)' },
-    { date: '2024-11-3', name: 'Bhai Dooj' },
+    { date: '2024-11-02', name: 'New Year (India)' },
+    { date: '2024-11-03', name: 'Bhai Dooj' },
   ];
 }
